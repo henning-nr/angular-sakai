2013637a8c4caa17bf278221f790ae82874ba791
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2024-05-22 17:22:29 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCpq5oC1hEb7S0Vk0mwWVHHGfN6FfyG2IU",
-    authDomain: "crud-angular-d33fb.firebaseapp.com",
-    databaseURL: "https://crud-angular-d33fb-default-rtdb.firebaseio.com",
-    projectId: "crud-angular-d33fb",
-    storageBucket: "crud-angular-d33fb.appspot.com",
-    messagingSenderId: "851254715092",
-    appId: "1:851254715092:web:732beee8177c0920e35141"
-  }
+  production: true
 };
