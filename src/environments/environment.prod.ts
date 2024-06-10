0dc3575fe832bbcc33fbe9ac11314c1f3a82a056
julianosfreitas <julianosfreitas@yahoo.com>
2024-06-10 19:58:33 -0300

    NEW COMMIT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDZZhsF0pntqvFwPBVcl5V8iVYjdgPHYjs',
+    authDomain: 'pizzariaromanova.firebaseapp.com',
+    projectId: 'pizzariaromanova',
+    storageBucket: 'pizzariaromanova.appspot.com',
+    messagingSenderId: '804919420112',
+    appId: '804919420112:web:1577b338b92509c7ad6ad5'
+  }
 };
