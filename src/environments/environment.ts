ebede0389efa7c14243c68bb5b85b4e4247b6723
CabralMKL <118760691+CabralMKL@users.noreply.github.com>
2024-06-12 14:29:41 -0300

    resolvendo bug de compilação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+    databaseURL: process.env['FIREBASE_DATABASE_URL'],
+    projectId: process.env['FIREBASE_PROJECT_ID'],
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+    appId: process.env['FIREBASE_APP_ID'],
+    measurementId: process.env['FIREBASE_MEASUREMENT_ID']
   }
 };
