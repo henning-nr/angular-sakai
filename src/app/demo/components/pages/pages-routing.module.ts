df01b1fe828c1e4a6d8f07033486a9651630a46f
julianosfreitas <julianosfreitas@yahoo.com>
2024-06-18 02:23:00 -0300

    Novos Componentes do Menu

--- src/app/demo/components/pages/pages-routing.module.ts
+++ src/app/demo/components/pages/pages-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [RouterModule.forChild([
         { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
+        { path: 'pizzaria', loadChildren: () => import('./crud/pizzaria.module').then(m => m.CrudModule) },
         { path: '**', redirectTo: '/notfound' }
     ])],
     exports: [RouterModule]
--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -60,50 +60,19 @@ export class AppMenuComponent implements OnInit {
                 icon: 'pi pi-fw pi-briefcase',
                 items: [
                     {
-                        label: 'Landing',
-                        icon: 'pi pi-fw pi-globe',
-                        routerLink: ['/landing']
-                    },
-                    {
-                        label: 'Auth',
+                        label: 'Cliente',
                         icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Crud',
-                        icon: 'pi pi-fw pi-pencil',
                         routerLink: ['/pages/crud']
                     },
                     {
-                        label: 'Timeline',
-                        icon: 'pi pi-fw pi-calendar',
-                        routerLink: ['/pages/timeline']
+                        label: 'Pedido',
+                        icon: 'pi pi-fw pi-list-check',
+                        routerLink: ['/pages/crud']
                     },
                     {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
+                        label: 'Produto',
+                        icon: 'pi pi-fw pi-cart-plus',
+                        routerLink: ['/pages/crud']
                     },
                 ]
             },
