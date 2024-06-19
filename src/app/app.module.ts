b8b41cbaf860e33c4c3b93ab9e3578a782a92bb2
Lucianomlinx <lucianomlins123@gmail.com>
2024-06-19 03:35:14 -0300

    tentativa de crud com Raphael tentamos ):

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,22 @@ import { AppLayoutComponent } from './layout/app.layout.component';
 import { ClientComponent } from './demo/components/pages/client/client.component';
 import { OrderComponent } from './demo/components/pages/order/order.component';
 import { ProductComponent } from './demo/components/pages/product/product.component';
-import { HttpClientModule } from '@angular/common/http';
+
+// Importar AngularFire módulos necessários
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +39,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
