8362188e63fbb9e2795fabdd88a3f738085e894f
juliasantosslv <julia.santoss004@gmail.com>
2024-06-19 14:14:55 -0300

    correcao

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCv1CBV773ZuQg3CaRnz6dQi56OO0vZXMs',
-    authDomain: 'melhor-aumigo.firebaseapp.com',
-    projectId: 'melhor-aumigo',
-    storageBucket: 'melhor-aumigo.appspot.com',
-    messagingSenderId: '817473555549',
-    appId: '1:817473555549:web:776cdda1addf2ee196d1b4'
+   
   }
 };
